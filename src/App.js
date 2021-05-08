59845281434815f9bf0f53ce9ff2fc022dc0c4d4
Anoop Chandran <anoopkcn@gmail.com>
2021-05-09 00:16:32 +0200

    Switch to hash router

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { VStack } from '@chakra-ui/layout';
 import Home from './components/home';
 import Posts from './components/posts'
 import RenderPost from './components/render-post'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <VStack>
           <Header />
@@ -21,6 +21,6 @@ export default function App() {
           <Footer />
         </VStack>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
